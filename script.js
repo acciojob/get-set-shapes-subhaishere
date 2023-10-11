48974425cbf2ba1c55ab4e457956275d75837f5e
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-10-11 20:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 //complete this code
 class Rectangle {
-	constructor(width,height){
-		this.width=width;
-		this.height=height;
-	}
-	getWidth(){
-		return this.width;
-	}
-	getHeight(){
-return this.height;
-	}
-	getArea(){
-		return width*height;
-	}
+    constructor(width, height){
+        this.width=width;
+        this.height=height;
+    }
+    get getArea(){
+        return (width,height);
+    }
 }
 
 class Square extends Animal {
-	getPerimeter(){
-		return 2*(l+b);
-	}
+    constructor(width,height){
+        super();
+        this.width=width;
+        this.height=height;
+    }
+    getPerimeter(){
+        //perimeter=4*(a*a)
+        let result=4*(width*width);
+        return result;
+    }
 }
+
+let result=new Rectangle(4,5);
+console.log(result.getPerimeter);
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
